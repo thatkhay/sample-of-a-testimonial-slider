6517ffde81b2538de5be36b23624caa8c4f14383
thatkhay <johnkingsleyegeonu@gmail.com>
2023-05-15 19:32:26 +0100

    css edit

--- slider.js
+++ slider.js
@@ -10,21 +10,27 @@ slideButton.addEventListener('click', slide);
 function slide() {
     if (imgIndex === 0) {
         sliderImg.src = './image-tanya.jpg';
-        bio.textContent = 'Paragraph 1';
-        bioName.textContent = 'Name 1';
+        bio.textContent = ` “ I’ve been interested in coding for a while but never taken the jump, until now. 
+        I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
+        excited about the future. ”`;
+        bioName.textContent = 'Tanya Sinclair';
         imgIndex = 1;
     } else if (imgIndex === 1) {
         sliderImg.src = './image-john.jpg';
-        bio.textContent = 'Paragraph 2';
-        bioName.textContent = 'Name 2';
+        bio.textContent =`   “ If you want to lay the best foundation possible I’d recommend taking this course. 
+        The depth the instructors go into is incredible. I now feel so confident about 
+        starting up as a professional developer. ”`;
+        bioName.textContent = 'John Tarkpor';
         imgIndex = 0;
     }
 }
 
 window.addEventListener('DOMContentLoaded', function() {
     sliderImg.src = './image-john.jpg';
-    bio.textContent = 'Paragraph 2';
-    bioName.textContent = 'Name 2';
+    bio.textContent = `   “ If you want to lay the best foundation possible I’d recommend taking this course. 
+    The depth the instructors go into is incredible. I now feel so confident about 
+    starting up as a professional developer. ”`;
+    bioName.textContent = 'John Tarkpor';
     imgIndex = 0;
 });
 
